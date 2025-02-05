a19d112a69f00255dfc8692884364d9bb5a4c2fe
fran-dawbaza <fgargar092@g.educaand.es>
2025-02-05 10:27:55 +0100

    feb 2

---
+++ tema3/js/programacionFuncional.js
@@ -0,0 +1,220 @@
+
+const coches = [
+    {'marca' : 'seat', 'modelo': 'panda', 'año': 1983, 'color': 'blanco', 'motor': 'gasolina'},
+    {'marca' : 'ford', 'modelo': 'mondeo', 'año': 2019, 'color': 'rojo', 'motor': 'diesel'},
+    {'marca'  : 'toyota', 'modelo': 'avensis', 'año': 2019, 'color': 'negro', 'motor': 'híbrido'},
+    {'marca'  : 'ford', 'modelo': 'fiesta', 'año': 1996, 'color': 'blanco', 'motor': 'diesel'},
+    {'marca'  : 'opel', 'modelo': 'astra', 'año': 2006, 'color': 'rojo', 'motor': 'diesel'},
+    {'marca'  : 'skoda', 'modelo': 'fabia', 'año': 1999, 'color': 'azul', 'motor': 'diesel'},
+    {'marca' : 'seat', 'modelo': 'leon', 'año': 2020, 'color': 'gris', 'motor': 'diesel'},
+    {'marca' : 'seat', 'modelo': 'ibiza', 'año': 2019, 'color': 'negro', 'motor': 'gasolina'}
+  ];
+
+// Ordenamos los coches por marca y modelo
+// puedo hacer una copia previamente para que no ordene "coches"
+
+const cochesOrdenados = [...coches]; // copio las referencias
+
+const ordenaPorMarcaYModelo = (a,b) => {
+    if (a.marca > b.marca)
+        return 1;
+    else if (a.marca < b.marca)
+        return -1;
+    else if (a.modelo > b.modelo)
+        return 1;
+    else if (a.modelo < b.modelo)
+        return -1;
+    else
+        return 0;
+};
+cochesOrdenados.sort(ordenaPorMarcaYModelo);
+
+
+
+console.log(coches);
+console.log(cochesOrdenados);
+
+// Coches antiguos (anteriores a 2000)
+
+const esAntiguo = coche => coche['año'] < 2000;
+
+const cochesAntiguos = coches.filter(esAntiguo);
+
+const esBlanco = coche => coche.color === 'blanco';
+
+console.log(cochesAntiguos);
+
+const cochesAntiguosYBlancos = coches
+                                  .filter(esAntiguo)
+                                  .filter(esBlanco);
+console.log(cochesAntiguosYBlancos);
+
+// Array con los años de venta de los coches
+
+const aniosVenta = coches.map(coche => coche['año']);
+console.log(aniosVenta);
+
+const hazLI = c => `
+                        <li>
+                          <strong>${c.marca}</strong> 
+                          ${c.modelo} 
+                          <i>${c['año']}</i>
+                        </li>`;
+
+document.getElementById('lista-coches').innerHTML = cochesOrdenados.map(hazLI).join('');
+/*
+
+// Media (o promedio) de los años de venta de los coches
+*/
+
+const funcionReductoraSuma = (acumulador,coche)=>{
+  //console.log(acumulador);
+  return acumulador+coche['año'];
+}
+
+const mediaAniosVenta = coches.reduce(funcionReductoraSuma,0)/coches.length;
+
+console.log(mediaAniosVenta);
+
+
+
+const mediaVentaCoches = coches.reduce((ac,coche, posicion)=>{
+    if (posicion === coches.length - 1) {
+        return (ac+coche['año']) / coches.length; 
+    }
+    return ac+coche['año'];
+}, 0);
+
+console.log ('del tirón: ' + mediaVentaCoches);
+
+/*
+Usando programación funcional (filter, map, reduce) y 
+así como otras funciones de arrays resuelve las siguientes 
+cuestiones y prueba su funcionamiento. 
+Como se trata de programación funcional, 
+no debes usar ningún tipo de bucle:
+
+- Dado un array como el siguiente:
+*/
+const pedidos = [
+  { id: 1, cliente: 'Pablito', items: ['Producto 1', 'Producto 2'] },
+  { id: 2, cliente: 'Miguelón', items: ['Producto 3', 'Producto 4'] },
+  { id: 3, cliente: 'Ricardín', items: ['Producto 5'] },
+  { id: 4, cliente: 'Pablito', items: ['Producto 2', 'Producto 4'] },
+  { id: 5, cliente: 'Ruperta', items: ['Producto 3'] }
+];
+// a) Obtener un array con los pedidos que contienen 
+// el ítem 'Producto 3' y ordena los pedidos por el nombre de cliente.
+
+const tieneP3 = p=>p.items.includes('Producto 3');
+const comparaPorCliente = (a,b)=>(a.cliente>b.cliente)?1:-1;
+
+const pedidosConP3 = pedidos
+                        .filter(tieneP3)
+                        .sort(comparaPorCliente);
+
+console.log('Pedidos con Producto 3 ordenados: ', pedidosConP3);
+
+// b) Obtener un array con los ítems de los pedidos 
+// del cliente 'Pablito'. (Pista puedes usar filter, 
+// luego map y luego flat para aplanar el resultado)
+
+const esDePablito = p=>p.cliente==='Pablito';
+
+const sinRepetir = (item,indice,items)=>items.indexOf(item)===indice;
+
+const itemsDePablito = pedidos
+                            .filter(esDePablito)
+                            .map(p=>p.items)
+                            .flat()
+                            .filter(sinRepetir);
+console.log('Productos de Pablito:');
+console.log(itemsDePablito);
+
+//- Dado un array como el siguiente:
+
+const ventas = [
+  { fecha: '2021-12-29', cantidad: 100 },
+  { fecha: '2022-01-02', cantidad: 200 },
+  { fecha: '2022-01-03', cantidad: 350 },
+  { fecha: '2022-01-04', cantidad: 240 },
+  { fecha: '2022-02-05', cantidad: 500 }
+];
+//c) Obtén un array con las ventas de enero y febrero, 
+// ordenado por cantidad.
+const esDeEneroOFebrero = v=>{
+    const mes = +v.fecha.split('-')[1];
+    return mes===1 || mes === 2;
+};
+const comparaCantidad = (a,b)=>a.cantidad-b.cantidad;
+
+const ventasEneroYFebrero = ventas
+                                .filter(esDeEneroOFebrero)
+                                .sort(comparaCantidad);
+
+console.log(ventasEneroYFebrero);
+
+//d) Calcula el promedio de ventas del mes de enero.
+// (Pista haz un filter y luego reduce)
+
+const esDeEnero = v=> (new Date(v.fecha)).getMonth()===0; // enero es 0
+
+const media = (ac, v,i,ventasDeEnero) => (i<ventasDeEnero.length-1) ?
+                                             ac+v.cantidad : 
+                                             (ac+v.cantidad)/ventasDeEnero.length
+
+const promedioVentasEnero = ventas
+                                .filter(esDeEnero)
+                                .reduce(media, 0);
+
+console.log(promedioVentasEnero);                                
+//- Dado un array como el siguiente:
+
+const productos = [
+  { nombre: 'Producto 1', precio: 10, categoria: 'Categoria 1' },
+  { nombre: 'Producto 2', precio: 20, categoria: 'Categoria 1' },
+  { nombre: 'Producto 3', precio: 30, categoria: 'Categoria 2' },
+  { nombre: 'Producto 4', precio: 40, categoria: 'Categoria 2' },
+  { nombre: 'Producto 5', precio: 50, categoria: 'Categoria 3' }
+];
+//e) Obtén un array de productos sin precio, 
+//es decir, con solo las propiedades nombre y categoria.
+
+const productosSinPrecio = productos.map(p=>({
+  nombre: p.nombre,
+  categoria: p.categoria
+}));
+console.log(productosSinPrecio);
+
+/*
+f) crear un objeto con el nombre de cada categoría y 
+la cantidad de productos que pertenecen a esa categoría. 
+Por ejemplo, para este caso el objeto debe tener la siguiente forma:
+
+{
+  'Category 1': 2,
+  'Category 2': 2,
+  'Category 3': 1
+}
+//Pista: usa map para obtener los nombres de la categoria
+// y luego reduce para contar cada categoria.
+*/
+
+const numeroProductosPorCategoria = productos
+                  .map(p=>p.categoria)
+                  .reduce((ac,c)=>{
+                    const nuevoAcumulador = {...ac};
+                    nuevoAcumulador[c] = nuevoAcumulador[c] ? nuevoAcumulador[c] + 1: 1;
+                    return nuevoAcumulador;
+                  }
+                  ,{});
+
+console.log(numeroProductosPorCategoria);
+
+// lo mismo con programación imperativa:
+const nPPC = {};
+for (let p of productos)
+    nPPC[p.categoria] = nPPC[p.categoria] ? nPPC[p.categoria]+1:1;
+
+
+console.log(nPPC);
---
+++ tema3/juegoMemoria/jMemoria.js
@@ -0,0 +1,123 @@
+const $iniciar = document.getElementById('btnIniciar');
+const $tablero = document.getElementById('tablero');
+const $mensajes = document.getElementById('mensajes');
+
+
+let intentos, cartasVolteadas, temporizador;
+
+$iniciar.addEventListener('click', iniciarJuego);
+$tablero.addEventListener('click', manejaClickEnTablero);
+
+function iniciarJuego() {
+    $tablero.innerHTML = '';
+    $mensajes.textContent = '';
+    intentos = 0;
+    cartasVolteadas = [];
+    actualizarTemporizador(0);
+    actualizarIntentos();
+
+    const $dificultad = document.getElementById('selectDificultad');
+    const $tipo = document.getElementById('selectTipo');
+
+
+    let dimensiones = parseInt($dificultad.value);
+    let tipoJuego = $tipo.value;
+    $tablero.style.gridTemplateColumns = `repeat(${dimensiones}, 100px)`;
+
+    const totalCartas = dimensiones * dimensiones;
+    const totalPares = Math.floor(totalCartas / 2);
+    let valores;
+
+    if (tipoJuego === 'numeros') {
+        valores = Array.from({ length: totalPares }, (_, i) => i + 1);
+    } else {
+        valores = generarImagenes(tipoJuego, totalPares);
+    }
+
+    const mazo = [...valores, ...valores].slice(0, totalCartas);
+    mazo.sort(() => Math.random() - 0.5);
+
+    $tablero.innerHTML = mazo.map(valor=>`
+        <div class="carta" data-valor="${valor}">
+            ${tipoJuego==='numeros'?
+                `<span>${valor}</span>`:
+                `<img src="${valor}"/>`
+            }
+        </div>`).join('');
+
+    if (temporizador)
+        clearInterval(temporizador);
+
+    let tiempo = 0;
+    temporizador = setInterval(() => {
+        tiempo++;
+        actualizarTemporizador(tiempo);
+    }, 1000);
+}
+
+
+
+function generarImagenes(categoria, cantidad) {
+    const urls = [];
+    const comienzo = 1 + Math.floor(200*Math.random());
+    for (let i = 1; i <= cantidad; i++) {
+        urls.push(`https://loremflickr.com/100/100/${categoria}?lock=${i+comienzo}`);
+    }
+    return urls;
+}
+
+function actualizarTemporizador(tiempo) {
+    const $tiempo = document.getElementById('tiempo');
+    $tiempo.textContent = tiempo;
+}
+
+function actualizarIntentos() {
+    const $intentos = document.getElementById('intentos');
+    $intentos.textContent = intentos;
+}
+
+
+function manejaClickEnTablero(evento){
+    console.log(evento.target);
+
+    const $carta = evento.target.closest('.carta');
+
+    if (!$carta || $carta.classList.contains('volteada') || cartasVolteadas.length === 2) {
+        return;
+    }
+
+    $carta.classList.add('volteada');
+    cartasVolteadas.push($carta);
+
+    if (cartasVolteadas.length === 2) {
+        intentos++;
+        actualizarIntentos();
+        comprobarPareja();
+    }
+}
+
+
+
+function comprobarPareja() {
+    const [carta1, carta2] = cartasVolteadas;
+
+    if (carta1.dataset.valor === carta2.dataset.valor) {
+        carta1.classList.add('emparejada');
+        carta2.classList.add('emparejada');
+        cartasVolteadas = [];
+
+        if (document.getElementsByClassName('volteada').length === document.getElementsByClassName('carta').length){
+            clearInterval(temporizador);
+            $mensajes.textContent = `¡Felicidades! Terminaste en ${tiempo} segundos.`;
+        }
+    } else {
+        setTimeout(() => {
+            carta1.classList.remove('volteada');
+            carta2.classList.remove('volteada');
+            cartasVolteadas = [];
+        }, 1000);
+    }
+
+    
+}
+
